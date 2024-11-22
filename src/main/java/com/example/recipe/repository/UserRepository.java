6ab045b09086b5ca35c817465ab9b0f652dd5e4b
Carlie Stewart <ayu6cp@virginia.edu>
2024-11-22 13:28:28 -0500

    controller/repository

---
+++ src/main/java/com/example/recipe/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.recipe.repository;
+
+import com.example.recipe.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByFirstNameContaining(String first_name);
+}
